964e8c1dfed8029f71cf077ca111d5b130d9e659
_Shortman <78680166+The-Shortman@users.noreply.github.com>
2024-08-09 00:24:54 +0100

    Ore excavation vein distribution rebalancing, and update packwiz pack.toml version to v005

---
+++ kubejs/server_scripts/excavation_midgrade.js
@@ -0,0 +1,197 @@
+ServerEvents.recipes(e => {
+
+    /// ITEMS ///
+    
+    [
+
+        // Overworld
+
+        {
+            out: 'minecraft:raw_iron',
+            vein: 'createoreexcavation:ore_vein_type/mi_iron_ov',
+            ironid: 'createoreexcavation:mi_iron_ov/iron',
+            diamid: 'createoreexcavation:mi_iron_ov/diamond',
+            nethid: 'createoreexcavation:mi_iron_ov/netherite'
+        },
+        {
+            out: 'minecraft:raw_copper',
+            vein: 'createoreexcavation:ore_vein_type/mi_copper_ov',
+            ironid: 'createoreexcavation:mi_copper_ov/iron',
+            diamid: 'createoreexcavation:mi_copper_ov/diamond',
+            nethid: 'createoreexcavation:mi_copper_ov/netherite'
+        },
+        {
+            out: 'create:raw_zinc',
+            vein: 'createoreexcavation:ore_vein_type/mi_zinc_ov',
+            ironid: 'createoreexcavation:mi_zinc_ov/iron',
+            diamid: 'createoreexcavation:mi_zinc_ov/diamond',
+            nethid: 'createoreexcavation:mi_zinc_ov/netherite'
+        },
+        {
+            out: 'minecraft:lapis_lazuli',
+            vein: 'createoreexcavation:ore_vein_type/mi_lapis_ov',
+            ironid: 'createoreexcavation:mi_lapis_ov/iron',
+            diamid: 'createoreexcavation:mi_lapis_ov/diamond',
+            nethid: 'createoreexcavation:mi_lapis_ov/netherite'
+        },
+        {
+            out: 'mekanism:raw_tin',
+            vein: 'createoreexcavation:ore_vein_type/mi_tin_ov',
+            ironid: 'createoreexcavation:mi_tin_ov/iron',
+            diamid: 'createoreexcavation:mi_tin_ov/diamond',
+            nethid: 'createoreexcavation:mi_tin_ov/netherite'
+        },
+        {
+            out: 'create_new_age:thorium',
+            vein: 'createoreexcavation:ore_vein_type/mi_thorium_ov',
+            ironid: 'createoreexcavation:mi_thorium_ov/iron',
+            diamid: 'createoreexcavation:mi_thorium_ov/diamond',
+            nethid: 'createoreexcavation:mi_thorium_ov/netherite'
+        },
+        {
+            out: 'minecraft:redstone',
+            vein: 'createoreexcavation:ore_vein_type/mi_redstone_ov',
+            ironid: 'createoreexcavation:mi_redstone_ov/iron',
+            diamid: 'createoreexcavation:mi_redstone_ov/diamond',
+            nethid: 'createoreexcavation:mi_redstone_ov/netherite'
+        },
+        {
+            out: 'minecraft:raw_gold',
+            vein: 'createoreexcavation:ore_vein_type/mi_gold_ov',
+            ironid: 'createoreexcavation:mi_gold_ov/iron',
+            diamid: 'createoreexcavation:mi_gold_ov/diamond',
+            nethid: 'createoreexcavation:mi_gold_ov/netherite'
+        },
+        {
+            out: 'create_ore_excavation:raw_diamond',
+            vein: 'createoreexcavation:ore_vein_type/mi_diamond_ov',
+            ironid: 'createoreexcavation:mi_diamond_ov/iron',
+            diamid: 'createoreexcavation:mi_diamond_ov/diamond',
+            nethid: 'createoreexcavation:mi_diamond_ov/netherite'
+        },
+
+        // Nether
+
+        {
+            out: 'minecraft:quartz',
+            vein: 'createoreexcavation:ore_vein_type/mi_quartz_ne',
+            ironid: 'createoreexcavation:mi_quartz_ne/iron',
+            diamid: 'createoreexcavation:mi_quartz_ne/diamond',
+            nethid: 'createoreexcavation:mi_quartz_ne/netherite'
+        },
+        {
+            out: 'minecraft:raw_gold',
+            vein: 'createoreexcavation:ore_vein_type/mi_gold_ne',
+            ironid: 'createoreexcavation:mi_gold_ne/iron',
+            diamid: 'createoreexcavation:mi_gold_ne/diamond',
+            nethid: 'createoreexcavation:mi_gold_ne/netherite'
+        },
+        {
+            out: 'minecraft:ancient_debris',
+            vein: 'createoreexcavation:ore_vein_type/mi_debris_ne',
+            ironid: 'createoreexcavation:mi_debris_ne/iron',
+            diamid: 'createoreexcavation:mi_debris_ne/diamond',
+            nethid: 'createoreexcavation:mi_debris_ne/netherite'
+        },
+
+        // Moon
+
+        {
+            out: 'mekanism:raw_osmium',
+            vein: 'createoreexcavation:ore_vein_type/mi_osmium_mo',
+            ironid: 'createoreexcavation:mi_osmium_mo/iron',
+            diamid: 'createoreexcavation:mi_osmium_mo/diamond',
+            nethid: 'createoreexcavation:mi_osmium_mo/netherite'
+        },
+        {
+            out: 'ad_astra:raw_desh',
+            vein: 'createoreexcavation:ore_vein_type/mi_desh_mo',
+            ironid: 'createoreexcavation:mi_desh_mo/iron',
+            diamid: 'createoreexcavation:mi_desh_mo/diamond',
+            nethid: 'createoreexcavation:mi_desh_mo/netherite'
+        },
+        {
+            out: 'ad_astra:cheese',
+            vein: 'createoreexcavation:ore_vein_type/mi_cheese_mo',
+            ironid: 'createoreexcavation:mi_cheese_mo/iron',
+            diamid: 'createoreexcavation:mi_cheese_mo/diamond',
+            nethid: 'createoreexcavation:mi_cheese_mo/netherite'
+        },
+        {
+            out: 'minecraft:raw_iron',
+            vein: 'createoreexcavation:ore_vein_type/mi_iron_mo',
+            ironid: 'createoreexcavation:mi_iron_mo/iron',
+            diamid: 'createoreexcavation:mi_iron_mo/diamond',
+            nethid: 'createoreexcavation:mi_iron_mo/netherite'
+        },
+
+        // Mars
+
+        {
+            out: 'mekanism:raw_lead',
+            vein: 'createoreexcavation:ore_vein_type/mi_lead_ma',
+            ironid: 'createoreexcavation:mi_lead_ma/iron',
+            diamid: 'createoreexcavation:mi_lead_ma/diamond',
+            nethid: 'createoreexcavation:mi_lead_ma/netherite'
+        },
+        {
+            out: 'ad_astra:raw_ostrum',
+            vein: 'createoreexcavation:ore_vein_type/mi_ostrum_ma',
+            ironid: 'createoreexcavation:mi_ostrum_ma/iron',
+            diamid: 'createoreexcavation:mi_ostrum_ma/diamond',
+            nethid: 'createoreexcavation:mi_ostrum_ma/netherite'
+        },
+        {
+            out: 'minecraft:raw_iron',
+            vein: 'createoreexcavation:ore_vein_type/mi_iron_ma',
+            ironid: 'createoreexcavation:mi_iron_ma/iron',
+            diamid: 'createoreexcavation:mi_iron_ma/diamond',
+            nethid: 'createoreexcavation:mi_iron_ma/netherite'
+        },
+
+    ].forEach((drill) => {
+        e.recipes.createoreexcavation.drilling(drill.out, drill.vein, 120).drill('createoreexcavation:drill').id(drill.ironid)
+        e.recipes.createoreexcavation.drilling(drill.out, drill.vein, 72).drill('createoreexcavation:diamond_drill').id(drill.diamid)
+        e.recipes.createoreexcavation.drilling(drill.out, drill.vein, 24).drill('createoreexcavation:netherite_drill').id(drill.nethid)
+    });
+
+    /// FLUIDS ///
+
+    [
+        // Overworld
+
+        {
+            out: 'minecraft:lava 2000',
+            vein: 'createoreexcavation:ore_vein_type/mi_lava_ov',
+            ironid: 'createoreexcavation:mi_lava_ov/iron',
+            diamid: 'createoreexcavation:mi_lava_ov/diamond',
+            nethid: 'createoreexcavation:mi_lava_ov/netherite'
+        },
+
+        // Nether
+
+        // Moon
+
+        {
+            out: 'minecraft:lava 2000',
+            vein: 'createoreexcavation:ore_vein_type/mi_lava_mo',
+            ironid: 'createoreexcavation:mi_lava_mo/iron',
+            diamid: 'createoreexcavation:mi_lava_mo/diamond',
+            nethid: 'createoreexcavation:mi_lava_mo/netherite'
+        },
+
+        // Mars
+
+        {
+            out: 'minecraft:water 2000',
+            vein: 'createoreexcavation:ore_vein_type/mi_water_ma',
+            ironid: 'createoreexcavation:mi_water_ma/iron',
+            diamid: 'createoreexcavation:mi_water_ma/diamond',
+            nethid: 'createoreexcavation:mi_water_ma/netherite'
+        },
+    ].forEach((pump) => {
+        e.recipes.createoreexcavation.drilling(pump.out, pump.vein, 120).drill('createoreexcavation:drill').id(pump.ironid)
+        e.recipes.createoreexcavation.drilling(pump.out, pump.vein, 72).drill('createoreexcavation:diamond_drill').id(pump.diamid)
+        e.recipes.createoreexcavation.drilling(pump.out, pump.vein, 24).drill('createoreexcavation:netherite_drill').id(pump.nethid)
+    })
+})
