650e436b6fc1de7fcdf922508506c81afe9c9c4b
_Shortman <78680166+The-Shortman@users.noreply.github.com>
2024-08-29 16:12:19 +0100

    Move MM structure registry to mm.js

--- kubejs/server_scripts/mm.js
+++ kubejs/server_scripts/mm.js
@@ -1,3 +1,38 @@
+MMEvents.createStructures(e => {
+    e.create("mm:blast_furnace")
+        .controllerId("mm:blast_furnace_controller")
+        .name("Blast Furnace")
+        .layout(a => {
+            a.layer([
+                "1O1",
+                "I2I",
+                "1C1"
+            ]).layer([
+                "121",
+                "2 2",
+                "121"
+            ]).layer([
+                "121",
+                "2 2",
+                "1O1"
+            ]).layer([
+                " 2 ",
+                "2 2",
+                " 2 "
+            ]).key("1", {
+                block: "tfmg:fireproof_brick_reinforcement"
+            }).key("2", {
+                block: "tfmg:fireproof_bricks"
+            }).key("I", {
+                port: "mm:blast_furnace_input",
+                input: true
+            }).key("O", {
+                port: "mm:blast_furnace_output",
+                input: false
+            })
+        })
+});
+
 MMEvents.createProcesses(e => {
     e.create("mm:blast_furnace/steel")
         .structureId("planetsandportals:blast_furnace")
