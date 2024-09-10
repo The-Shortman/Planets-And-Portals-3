d823fcb88f3571ebc4a61db89ccb4353bb7e6456
_Shortman <78680166+The-Shortman@users.noreply.github.com>
2024-09-10 18:01:05 +0100

    Ponders for real (they work!!)

--- kubejs/client_scripts/ponders/registry.js
+++ kubejs/client_scripts/ponders/registry.js
@@ -9,27 +9,34 @@ Ponder.registry((e) => {
 				scene.idle(10);
 
 				for (let z = 0; z < 5; z++) {
-					scene.world.showSection([3, 1, z], Facing.DOWN);
+					scene.world.showSection([2, 1, z], Facing.DOWN);
 				scene.idle(3);
 				}
-				scene.text(30, "Item output port", [2, 1, 1]).colored(PonderPalette.OUTPUT).attachKeyFrame();
+				scene.text(40, "Item output port", [2, 1.5, 1.5])
+					.colored(PonderPalette.OUTPUT)
+					.attachKeyFrame();
 
-				scene.idle(5)
+				scene.idle(40)
 
 				for (let z = 0; z < 5; z++) {
-					scene.world.showSection([3, 2, z], Facing.DOWN);
+					scene.world.showSection([2, 2, z], Facing.DOWN);
 				scene.idle(3);
 				}
-				scene.text(30, "Controller block", [2, 2, 1]).colored(PonderPalette.MEDIUM).attachKeyFrame();
+				scene.text(60, "Controller block", [2, 2.5, 1.5])
+					.colored(PonderPalette.MEDIUM)
+					.attachKeyFrame();
 
-				scene.idle(5)
+				scene.idle(40)
 
 				for (let z = 0; z < 5; z++) {
-					scene.world.showSection([3, 2, z], Facing.DOWN);
+					scene.world.showSection([2, 3, z], Facing.DOWN);
 				scene.idle(3);
 				}
-				scene.text(30, "Fluid (byproduct) output port", [2, 3, 1]).colored(PonderPalette.OUTPUT).attachKeyFrame();
-				scene.text(30, "Item output port", [2, 3, 3]).colored(PonderPalette.INPUT);
+				scene.text(80, "Fluid (byproduct) output port", [2, 3.5, 1.5])
+					.colored(PonderPalette.OUTPUT)
+					.attachKeyFrame();
+				scene.text(80, "Item input port", [2, 3.5, 3.5])
+					.colored(PonderPalette.INPUT);
 			}
 		);
 
@@ -51,37 +58,42 @@ Ponder.registry((e) => {
 					}
 				}
 
-				scene.text(60, "Coke ovens can be placed side by side in long production lines.", [6, 3, 5]);
-				scene.idle(10);
+				scene.text(80, "Coke ovens can be placed side by side in long production lines.", [6, 3.5, 5.5]);
+				scene.idle(80);
 
 				scene.world.showSection([6, 5, 7, 10, 6, 11], Facing.DOWN);
-				scene.text(40, "Coal can be inseted from the back input port.", [6, 5, 7]).attachKeyFrame();
+				scene.text(120, "Coal can be inserted from the back input port.", [6, 5.5, 7.5])
+					.attachKeyFrame();
 				scene.idle(10);
 
 				scene.world.showSection([6, 1, 0, 10, 2, 4], Facing.SOUTH);
-				scene.text(40, "Coke coal can be extracted from the lower output port.", [6, 2, 4]);
-				scene.idle(10);
+				scene.text(120, "Coke coal can be extracted from the lower output port.", [6, 2.5, 4.5]);
+				scene.idle(110);
 
-				for (let x = 1; x < 4; z++) {
-					for (let z = 5; z < 8; x++) {
+				for (let x = 1; x < 4; x++) {
+					for (let z = 5; z < 8; z++) {
 						for (let y = 1; y < 5; y++) {
 							scene.world.showSection([x, y, z], Facing.DOWN);
-							scene.idle(1)
+							scene.idle(1);
 						}
 					}
 				}
 
 				scene.world.showSection([0, 5, 4, 10, 7, 5], Facing.DOWN);
-				scene.text(40, "Creosote is also produced in the Ovens.", [6, 4, 5]).attachKeyFrame();
-				scene.idle(5);
-				scene.text(40, "This can be stored for usage in crafting...", [1, 3, 5]);
-				scene.text(40, "...or burned off with a Flarestack.", [1, 7, 5]);
-				scene.idle(10);
+				scene.text(80, "Creosote is also produced in the Ovens.", [6, 4.5, 5.5])
+					.attachKeyFrame();
+				scene.idle(80);
+				scene.text(60, "This can be stored for usage in crafting...", [1, 3.5, 5.5]);
+				scene.idle(70);
+				scene.text(60, "...or burned off with a Flarestack.", [1, 7.5, 5.5]);
+				scene.idle(70);
 
 				scene.world.showSection([6, 4, 4, 10, 4, 4], Facing.SOUTH);
 				scene.world.showSection([4, 4, 3, 10, 5, 3], Facing.SOUTH);
-				scene.text(30, "Carbon Dioxide is a waste product.", [4, 5, 3]).attachKeyFrame();
-				scene.text(50, "Best to release it into the atmosphere with an Exhaust.", [4, 5, 3]);
+				scene.text(60, "Carbon Dioxide is a waste product.", [4, 5.5, 3.5])
+					.attachKeyFrame();
+				scene.idle(70);
+				scene.text(80, "Best to release it into the atmosphere with an Exhaust.", [4, 5.5, 3.5]);
 			}
 		);
 	
@@ -91,8 +103,51 @@ Ponder.registry((e) => {
 			"Building the Blast Furnace",
 			"planetsandportals:blast_furnace_structure",
 			(scene, util) => {
-				scene.showStructure();
+				scene.showBasePlate();
+				scene.idle(10);
 
+				for (let z = 0; z < 4; z++) {
+					for (let x = 0; x < 4; x++) {
+						scene.world.showSection([x, 1, z], Facing.DOWN);
+						scene.idle(1);
+					}
+				}
+
+				scene.text(40, "Controller block", [2.5, 1.5, 1])
+					.colored(PonderPalette.MEDIUM);
+				scene.idle(50);
+				scene.text(40, "Item input", [1, 1.5, 2.5])
+					.colored(PonderPalette.INPUT);
+				scene.idle(50);
+				scene.text(40, "Item input", [3.5, 2, 2.5])
+					.colored(PonderPalette.INPUT);
+				scene.idle(50);
+				scene.text(40, "Fluid output", [2.5, 2, 3.5])
+					.colored(PonderPalette.OUTPUT);
+				scene.idle(50);
+
+				for (let z = 0; z < 4; z++) {
+					for (let x = 0; x < 4; x++) {
+						scene.world.showSection([x, 2, z], Facing.DOWN);
+					}
+					scene.idle(1);
+				}
+				scene.idle(20);
+
+				for (let z = 0; z < 4; z++) {
+					for (let x = 0; x < 4; x++) {
+						scene.world.showSection([x, 3, z], Facing.DOWN);
+					}
+					scene.idle(1);
+				}
+				scene.idle(20);
+
+				for (let z = 0; z < 4; z++) {
+					for (let x = 0; x < 4; x++) {
+						scene.world.showSection([x, 4, z], Facing.DOWN);
+					}
+					scene.idle(1);
+				}
 			}
 		);
 
@@ -102,8 +157,52 @@ Ponder.registry((e) => {
 			"Using the Blast Furnace",
 			"planetsandportals:blast_furnace_usage",
 			(scene, util) => {
-				scene.showStructure();
+				scene.showBasePlate();
+				scene.idle(10);
 
+				scene.world.showSection([0, 1, 0, 10, 1, 3], Facing.DOWN);
+				scene.world.showSection([3, 1, 4, 7, 1, 5], Facing.DOWN);
+				scene.idle(5);
+
+				for (let x = 4; x < 7; x++) {
+					for (let z = 2; z < 5; z++) {
+						for (let y = 2; y < 6; y++) {
+							scene.world.showSection([x, y, z], Facing.DOWN);
+							scene.idle(1);
+						}
+					}
+				}
+				scene.idle(10);
+
+				scene.world.showSection([3, 2, 3], Facing.EAST);
+				scene.world.showSection([7, 2, 3], Facing.WEST);
+				scene.idle(10);
+
+				scene.text(100, "Input blasting mixture in one side, and coal coke dust in the other.", [3.5, 2.5, 3.5])
+					.colored(PonderPalette.INPUT);
+				scene.idle(110);
+
+				scene.world.hideSection([4, 2, 2, 6, 5, 4], Facing.NORTH);
+				scene.idle(5);
+				scene.world.showSection([0, 1, 6, 10, 1, 10], Facing.DOWN);
+				scene.world.showSection([2, 1, 4, 2, 1, 5], Facing.DOWN);
+				scene.idle(10);
+
+				scene.world.showSection([4, 2, 7, 5, 4, 7], Facing.NORTH);
+				scene.idle(10);
+				scene.text(120, "Output fluid can be cast into solid metal with a casting basin and casting spout.", [5, 4.5, 7.5])
+					.colored(PonderPalette.OUTPUT)
+					.attachKeyFrame();
+				scene.idle(130);
+
+				scene.world.showSection([5, 2, 5, 5, 6, 6], Facing.WEST);
+				scene.idle(10);
+				scene.text(800, "Fluid can be pumped out from the rear port.", [5, 2.5, 5.5])
+					.colored(PonderPalette.OUTPUT)
+					.attachKeyFrame();
+				scene.idle(90);
+
+				scene.world.showSection([4, 2, 2, 6, 5, 4], Facing.SOUTH);
 			}
 		);
 	
@@ -113,8 +212,51 @@ Ponder.registry((e) => {
 			"Building the Steel Furnace",
 			"planetsandportals:steel_furnace_structure",
 			(scene, util) => {
-				scene.showStructure();
+				scene.showBasePlate();
+				scene.idle(10);
 
+				for (let z = 0; z < 4; z++) {
+					for (let x = 0; x < 4; x++) {
+						scene.world.showSection([x, 1, z], Facing.DOWN);
+						scene.idle(1);
+					}
+				}
+
+				scene.text(40, "Controller block", [2.5, 1.5, 1])
+					.colored(PonderPalette.MEDIUM);
+				scene.idle(50);
+				scene.text(40, "Item input", [1, 1.5, 2.5])
+					.colored(PonderPalette.INPUT);
+				scene.idle(50);
+				scene.text(40, "Item input", [3.5, 2, 2.5])
+					.colored(PonderPalette.INPUT);
+				scene.idle(50);
+				scene.text(40, "Fluid output", [2.5, 2, 3.5])
+					.colored(PonderPalette.OUTPUT);
+				scene.idle(50);
+
+				for (let z = 0; z < 4; z++) {
+					for (let x = 0; x < 4; x++) {
+						scene.world.showSection([x, 2, z], Facing.DOWN);
+					}
+					scene.idle(1);
+				}
+				scene.idle(20);
+
+				for (let z = 0; z < 4; z++) {
+					for (let x = 0; x < 4; x++) {
+						scene.world.showSection([x, 3, z], Facing.DOWN);
+					}
+					scene.idle(1);
+				}
+				scene.idle(20);
+
+				for (let z = 0; z < 4; z++) {
+					for (let x = 0; x < 4; x++) {
+						scene.world.showSection([x, 4, z], Facing.DOWN);
+					}
+					scene.idle(1);
+				}
 			}
 		);
 
@@ -124,8 +266,63 @@ Ponder.registry((e) => {
 			"Using the Steel Furnace",
 			"planetsandportals:steel_furnace_usage",
 			(scene, util) => {
-				scene.showStructure();
+				scene.world.setBlock([5, 1, 2], "tfmg:steel_casing", false)
+				scene.showBasePlate();
+				scene.idle(10);
 
+				scene.world.showSection([0, 1, 0, 10, 1, 3], Facing.DOWN);
+				scene.world.showSection([3, 1, 4, 7, 1, 5], Facing.DOWN);
+				scene.idle(5);
+
+				for (let x = 4; x < 7; x++) {
+					for (let z = 2; z < 5; z++) {
+						for (let y = 2; y < 6; y++) {
+							scene.world.showSection([x, y, z], Facing.DOWN);
+							scene.idle(1);
+						}
+					}
+				}
+				scene.idle(10);
+
+				scene.world.showSection([3, 2, 3], Facing.EAST);
+				scene.world.showSection([7, 2, 3], Facing.WEST);
+				scene.idle(10);
+
+				scene.text(100, "Input blasting mixture in one side, and coal coke dust in the other.", [3.5, 2.5, 3.5])
+					.colored(PonderPalette.INPUT);
+				scene.idle(110);
+
+				scene.text(100, "The steel furnace is slightly faster than the blast furnace.", [3.5, 2.5, 3.5])
+					.colored(PonderPalette.INPUT)
+					.attachKeyFrame();
+				scene.idle(110);
+
+				scene.text(120, "The steel variant can also manufacture Desh and Osmium. Check EMI for their recipes.", [3.5, 2.5, 3.5])
+					.colored(PonderPalette.MEDIUM)
+					.attachKeyFrame();
+				scene.idle(130);
+
+				scene.world.hideSection([4, 2, 2, 6, 5, 4], Facing.NORTH);
+				scene.idle(5);
+				scene.world.showSection([0, 1, 6, 10, 1, 10], Facing.DOWN);
+				scene.world.showSection([2, 1, 4, 2, 1, 5], Facing.DOWN);
+				scene.idle(10);
+
+				scene.world.showSection([4, 2, 7, 5, 4, 7], Facing.NORTH);
+				scene.idle(10);
+				scene.text(120, "Output fluid can be cast into solid metal with a casting basin and casting spout.", [5, 4.5, 7.5])
+					.colored(PonderPalette.OUTPUT)
+					.attachKeyFrame();
+				scene.idle(130);
+
+				scene.world.showSection([5, 2, 5, 5, 6, 6], Facing.WEST);
+				scene.idle(10);
+				scene.text(800, "Fluid can be pumped out from the rear port.", [5, 2.5, 5.5])
+					.colored(PonderPalette.OUTPUT)
+					.attachKeyFrame();
+				scene.idle(90);
+
+				scene.world.showSection([4, 2, 2, 6, 5, 4], Facing.SOUTH);
 			}
 		);
 })
