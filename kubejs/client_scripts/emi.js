cea1cc3c97ae9610d872d75c2afed518b8585c87
The-Shortman <78680166+The-Shortman@users.noreply.github.com>
2025-01-16 18:51:54 +0000

    Attempt to hide/show EMI entries again

--- kubejs/client_scripts/emi.js
+++ kubejs/client_scripts/emi.js
@@ -1,5 +1,15 @@
-RecipeViewerEvents.addEntries('item', event => {
-    event.add('create:crushed_raw_aluminum')
-  }
-)
+JEIEvents.addItems('item', event => {
+    [
+		["create:crushed_raw_aluminum"]
+    ].forEach((recipe) => {
+		event.add(recipe[0])
+    })
+})
 
+JEIEvents.hideItems('item', event => {
+    [
+		["planetsandportals:end_prevention_placeholder"]
+    ].forEach((recipe) => {
+		event.hide(recipe[0])
+    })
+})
