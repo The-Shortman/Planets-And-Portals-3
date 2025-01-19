4b3ad397da4deda75f885163df505ce1b25667fe
The-Shortman <78680166+The-Shortman@users.noreply.github.com>
2025-01-19 00:09:37 +0000

    Ore vein finder pulse wave

--- kubejs/startup_scripts/startup.js
+++ kubejs/startup_scripts/startup.js
@@ -1,1 +1,5 @@
 Platform.mods.kubejs.name = 'Planets and Portals 3'
+
+// startup script
+global.onLivingSwapItemsEvent = function(e){} // placeholder
+ForgeEvents.onEvent('net.minecraftforge.event.entity.living.LivingSwapItemsEvent',  global.onLivingSwapItemsEvent);
