8bea4a6e2edbe3ca2416a8c977d7f340830430f6
The-Shortman <78680166+The-Shortman@users.noreply.github.com>
2025-03-26 07:54:45 +0000

    Only replace diamond tools to need rough diamond in crafting

--- kubejs/server_scripts/recipes/bulk_replacements/inputreplacements.js
+++ kubejs/server_scripts/recipes/bulk_replacements/inputreplacements.js
@@ -31,7 +31,17 @@ ServerEvents.recipes(e => {
             replace: 'mekanism:ingot_osmium'
         },
         {
-            filter: {mod: 'minecraft'},
+            filter: {output: [
+                'minecraft:diamond_shovel',
+                'minecraft:diamond_pickaxe',
+                'minecraft:diamond_axe',
+                'minecraft:diamond_hoe',
+                'minecraft:diamond_sword',
+                'minecraft:diamond_helmet',
+                'minecraft:diamond_chestplate',
+                'minecraft:diamond_leggings',
+                'minecraft:diamond_boots'
+            ]},
             remove: 'minecraft:diamond',
             replace: 'planetsandportals:rough_diamond'
         },
