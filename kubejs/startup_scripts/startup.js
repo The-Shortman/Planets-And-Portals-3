99879fde5005fcfc453a100054825fc5e54ae2a0
The-Shortman <78680166+The-Shortman@users.noreply.github.com>
2025-12-14 18:55:15 +0000

    Remove KubeJS reassignment

--- kubejs/startup_scripts/startup.js
+++ kubejs/startup_scripts/startup.js
@@ -1,5 +1,3 @@
-Platform.mods.kubejs.name = "Planets and Portals 3";
-
 // unused event to detect when a player swaps main hand item
 
 global.onLivingSwapItemsEvent = function (event) {}; // placeholder
