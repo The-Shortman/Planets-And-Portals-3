7551b9ee456a5d90cd1acd4a858f1abad8a7efe5
The-Shortman <78680166+The-Shortman@users.noreply.github.com>
2025-12-21 13:09:21 +0000

    Warty compacting

---
+++ kubejs/server_scripts/recipes/create/compacting.js
@@ -0,0 +1,47 @@
+(function createCompactingRecipes() {
+  const { BUCKET, BLOCK, INGOT, NUGGET } = global.fluids;
+
+  ServerEvents.recipes((event) => {
+    const compactingRecipes = [
+      {
+        input: "minecraft:nether_wart",
+        output: { fluid: "planetsandportals:wart_juice", amount: BUCKET / 10 },
+        heat: "heated",
+        id: "wart_juice",
+      },
+      {
+        input: [
+          { fluid: "planetsandportals:wart_juice", amount: BUCKET / 2 },
+          { fluid: "create:honey", amount: BUCKET / 2 },
+        ],
+        output: "planetsandportals:sticky_wart",
+        id: "sticky_wart",
+      },
+      {
+        input: "planetsandportals:stony_wart",
+        output: { fluid: "minecraft:lava", amount: BUCKET / 2 },
+        heat: "heated",
+        id: "lava_from_stony_wart",
+      },
+      {
+        input: { fluid: "minecraft:lava", amount: BUCKET },
+        output: "minecraft:andesite",
+        id: "andesite_from_lava",
+      },
+    ];
+
+    compactingRecipes.forEach((recipe) => {
+      // kube throws a fit if you give heatRequirement an empty value unlike in 1.18.2
+      if (recipe.heat != null) {
+        event.recipes.create
+          .compacting(recipe.output, recipe.input)
+          .heatRequirement(recipe.heat)
+          .id(`kubejs:create/compacting/${recipe.id}`);
+      } else {
+        event.recipes.create
+          .compacting(recipe.output, recipe.input)
+          .id(`kubejs:create/compacting/${recipe.id}`);
+      }
+    });
+  });
+})();
