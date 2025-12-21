d2d26e625a7bb505b9c9d5db4f8866adb8dd56bf
The-Shortman <78680166+The-Shortman@users.noreply.github.com>
2025-12-21 13:08:20 +0000

    Stony wart from wart juice

--- kubejs/server_scripts/recipes/create/mixing.js
+++ kubejs/server_scripts/recipes/create/mixing.js
@@ -3,25 +3,34 @@
   ServerEvents.recipes((event) => {
     const mixingRecipes = [
       {
-        inputs: ["planetsandportals:grout", "planetsandportals:andesite_dust"],
-        outputs: "2x create:andesite_alloy",
+        input: ["planetsandportals:grout", "planetsandportals:andesite_dust"],
+        output: "2x create:andesite_alloy",
         id: "andesite_alloy_efficient",
       },
       {
-        inputs: [
+        input: [
           "create:crushed_raw_zinc",
           Item.of("create:crushed_raw_copper", 2),
         ],
-        outputs: Item.of("planetsandportals:brass_mixture", 3),
+        output: Item.of("planetsandportals:brass_mixture", 3),
         id: "brass_mixture",
       },
       {
-        inputs: [
+        input: [
           Item.of("mekanism:dust_quartz", 1),
           Item.of("minecraft:redstone", 3),
         ],
-        outputs: Item.of("planetsandportals:rose_quartz_dust", 4),
+        output: Item.of("planetsandportals:rose_quartz_dust", 4),
         id: "rose_quartz_dust",
+      },
+      {
+        input: [
+          { fluid: "planetsandportals:wart_juice", amount: BUCKET / 2 },
+          Item.of("minecraft:cobblestone", 8),
+        ],
+        output: "planetsandportals:stony_wart",
+        heat: "heated",
+        id: "stony_wart",
       },
     ];
 
@@ -29,13 +38,13 @@
       // kube throws a fit if you give heatRequirement an empty value unlike in 1.18.2
       if (recipe.heat != null) {
         event.recipes.create
-          .mixing(recipe.outputs, recipe.inputs)
+          .mixing(recipe.output, recipe.input)
           .heatRequirement(recipe.heat)
           .processingTime(recipe.time ?? 100)
           .id(`kubejs:create/mixing/${recipe.id}`);
       } else {
         event.recipes.create
-          .mixing(recipe.outputs, recipe.inputs)
+          .mixing(recipe.output, recipe.input)
           .processingTime(recipe.time ?? 100)
           .id(`kubejs:create/mixing/${recipe.id}`);
       }
