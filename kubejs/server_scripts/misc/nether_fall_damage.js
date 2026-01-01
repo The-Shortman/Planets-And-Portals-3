09139ce4711effbe3a496b75fa453a7e0469714a
The-Shortman <78680166+The-Shortman@users.noreply.github.com>
2026-01-01 14:46:50 +0000

    Remove debug logging from fall damage

--- kubejs/server_scripts/misc/nether_fall_damage.js
+++ kubejs/server_scripts/misc/nether_fall_damage.js
@@ -6,9 +6,7 @@
     if (!event.entity.isPlayer()) return;
 
     if (player.level.dimension == "minecraft:the_nether") {
-      console.log(`Player took ${source.type().msgId()} damage in Nether`);
       if (source.type().msgId() == "fall") {
-        console.log("Player fell in Nether");
         event.cancel();
       }
     }
