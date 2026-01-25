c99af2865685b86b9293916c7068f46bf1b73bee
The-Shortman <78680166+The-Shortman@users.noreply.github.com>
2026-01-25 14:18:42 +0000

    Scrap old ponders

--- kubejs/client_scripts/ponders/tags.js
+++ kubejs/client_scripts/ponders/tags.js
@@ -4,16 +4,9 @@
       {
         id: "planetsandportals:tier_1_machines",
         icon: "minecraft:paper",
-        name: "Book 1 Multiblocks",
-        desc: "Placeholder",
+        name: "Early Game Multiblocks",
+        desc: "Ponders coming soon!",
         items: "minecraft:paper",
-      },
-      {
-        id: "planetsandportals:tier_2_machines",
-        icon: "mm:steel_furnace_controller",
-        name: "Book 2 Multiblocks",
-        desc: "Machines designed for early industry",
-        items: ["mm:steel_furnace_controller", "mm:fuel_mixer_controller"],
       },
     ].forEach((ponder) => {
       event.createTag(
