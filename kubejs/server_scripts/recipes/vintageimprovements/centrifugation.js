03aa12446aad4a834f389c08dec5679ec5950e92
The-Shortman <78680166+The-Shortman@users.noreply.github.com>
2026-01-26 01:00:12 +0000

    Ethylene and propylene centrifuge recipes

--- kubejs/server_scripts/recipes/vintageimprovements/centrifugation.js
+++ kubejs/server_scripts/recipes/vintageimprovements/centrifugation.js
@@ -2,7 +2,18 @@
   ServerEvents.recipes((event) => {
     const { BUCKET, BOTTLE, BLOCK, INGOT, NUGGET } = global.fluids;
 
-    const centrifugationRecipes = [];
+    const centrifugationRecipes = [
+      {
+        input: { fluid: "tfmg:naphtha", amount: 20 },
+        output: [
+          { fluid: "tfmg:ethylene", amount: 10 },
+          { fluid: "tfmg:propylene", amount: 10 },
+        ],
+        time: 10,
+        minRPM: 128,
+        id: "ethylene_and_propylene",
+      },
+    ];
 
     centrifugationRecipes.forEach((recipe) => {
       event.recipes.vintageimprovements
