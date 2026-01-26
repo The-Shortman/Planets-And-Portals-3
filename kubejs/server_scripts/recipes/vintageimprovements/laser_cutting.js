fd5bd99a9752a68211d4b77baf7284ebb5de1370
The-Shortman <78680166+The-Shortman@users.noreply.github.com>
2026-01-26 14:43:07 +0000

    Rocket fin recipe

--- kubejs/server_scripts/recipes/vintageimprovements/laser_cutting.js
+++ kubejs/server_scripts/recipes/vintageimprovements/laser_cutting.js
@@ -1,13 +1,21 @@
 (function vintageimprovementsLaserCuttingRecipes() {
   ServerEvents.recipes((event) => {
-    const laserCuttingRecipes = [];
+    const laserCuttingRecipes = [
+      {
+        input: "tfmg:heavy_plate",
+        output: "ad_astra:rocket_fin",
+        energy: 4000,
+        chargeRate: 50,
+        id: "rocket_fin",
+      },
+    ];
 
     laserCuttingRecipes.forEach((recipe) => {
       event.recipes.vintageimprovements
         .laser_cutting(recipe.output, recipe.input)
         .energyCost(recipe.energy)
-        .maxChargeRate(recipe.chargeRate ?? recipe.energy / 10)
+        .maxChargeRate(recipe.chargeRate)
         .id(`kubejs:vintageimprovements/laser_cutting/${recipe.id}`);
     });
   });
-});
+})();
