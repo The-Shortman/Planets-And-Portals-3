e150c7379143acbe3a71dfcdc385f8f286c5bc14
The-Shortman <78680166+The-Shortman@users.noreply.github.com>
2026-02-08 11:05:03 +0000

    Slag from slag block recipe

--- kubejs/server_scripts/recipes/create/crushing.js
+++ kubejs/server_scripts/recipes/create/crushing.js
@@ -52,6 +52,11 @@
         id: "slag_from_slag_block",
       },
       {
+        input: "ppfmg:slag_bricks",
+        output: [Item.of("ppfmg:slag", 9)],
+        id: "slag_from_slag_bricks",
+      },
+      {
         input: "ppfmg:coal_coke",
         output: "ppfmg:coal_coke_dust",
         id: "coal_coke_dust",
